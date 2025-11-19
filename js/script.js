80860ad60e7114ae8b7b7af078bfb39f921b60d8
SwitchTia <oblasser.tatiana@gmail.com>
2025-11-19 18:42:00 +0100

    Fix: Last debug

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// DOM elements
 const cardGridElem = document.getElementById("card-grid");
 //console.log(cardGridElem);
 const overlayElem = document.getElementById("overlay");
@@ -8,45 +9,46 @@ const overlayContent = document.querySelector(".overlay-content");
 //console.log((overlayContent));
 
 
-//CREATE ELEMENT
-
-axios
-    .get("https://lanciweb.github.io/demo/api/pictures/")
-    .then((resp) => {
-        const cardsArray = resp.data;
-        //console.log(cardsArray);
-
-        printCard(cardsArray);
-    })
+// Global variable to store the active card
+let activeCard = null;
+let cardsArray = []; 
 
 
-//Function print card
-function printCard(cardsArray) {
-    cardsArray.forEach((currentCard) => {
-
-        const cardElem = createCard(currentCard);
-        //console.log(currentCard);
-        cardGridElem.append(cardElem);
+//Getting the card data:
+axios.get("https://lanciweb.github.io/demo/api/pictures/")
+    .then(resp => {
+        cardsArray = resp.data; 
+        printCards(cardsArray);
     });
 
+
+//Function that prints all cards:
+function printCards(cardsArray) {
+    cardsArray.forEach((cardData, index) => {
+        const cardElem = createCard(cardData, index);
+        cardGridElem.append(cardElem);
+    });
 }
 
-//Function create card
-function createCard(currentCard) {
+
+//Function that create a single card element:
+function createCard(currentCard, index) {
     const colElem = document.createElement("div");
     colElem.classList.add("col");
 
-    colElem.innerHTML =
-        `  
-                 <div class="card">
-                    <img class="pin" src="./img/pin.svg" alt="pin">
-                    <img class="card-img" src="${currentCard.url}" alt="Image of blog activities">
-                    <h3 class="card-date">${currentCard.date}</h3>
-                    <h2 class="card-title">${currentCard.title}</h2>
-                </div>
-            `;
-    colElem.addEventListener("click", function () {
-        fillOverlay(colElem);
+    colElem.innerHTML = `
+        <div class="card">
+            <img class="pin" src="./img/pin.svg" alt="pin">
+            <img class="card-img" src="${currentCard.url}" alt="Image of blog activities">
+            <h3 class="card-date">${currentCard.date}</h3>
+            <h2 class="card-title">${currentCard.title}</h2>
+        </div>
+    `;
+
+    //On click event sets this card as active and shows overlay:
+    colElem.addEventListener("click", () => {
+        activeCard = currentCard;      
+        fillOverlay(activeCard);   
         showOverlay();
     });
 
@@ -58,33 +60,27 @@ function createCard(currentCard) {
 
 //OVERLAY:
 
-//Function fill overlay with active card info
-function fillOverlay(cardElem) {
-    const imgSrc = cardElem.querySelector(".card-img").src;
-    const title = cardElem.querySelector(".card-title").textContent;
-    const date = cardElem.querySelector(".card-date").textContent;
-
-    const overlayContent = document.querySelector(".overlay-content");
+//Funtion that fills the overlay with the active card data:
+function fillOverlay(currentCard) {
     overlayContent.innerHTML = `
-        <img src="${imgSrc}" alt="${title}" style="width:100%; border-radius:10px; margin-top:10px;">
-        <h3 style="padding:10px; color:gray;">${date}</h3>
-        <h2 style="padding:10px;">${title}</h2>
+        <img class="card-img" src="${currentCard.url}" alt="Image of blog activities">
+        <h3 class="card-date">${currentCard.date}</h3>
+        <h2 class="card-title">${currentCard.title}</h2>
     `;
 }
 
-
-//Function to show the overlaid card
+//Function to show the overlaid card:
 function showOverlay() {
     overlayElem.classList.remove("hidden");
 }
 
-//On click the overlayed card dissapears (display-none)
+//On click on the X btn the overlayed card dissapears (display-none):
 closeBtnOverlay.addEventListener("click", function () {
     overlayElem.classList.add("hidden");
 });
 
 
-//Adding display none to the overlay card if any click is done around it, on background
+//Adding display none to the overlay card if any click is done around it, on background:
 overlayElem.addEventListener("click", function (event) {
     if (event.target === overlayElem) {  
         overlayElem.classList.add("hidden");
@@ -94,30 +90,130 @@ overlayElem.addEventListener("click", function (event) {
 
 
 
-//TEMPLATE LITERAL
 
+
+// const cardGridElem = document.getElementById("card-grid");
+// //console.log(cardGridElem);
+// const overlayElem = document.getElementById("overlay");
+// //console.log(overlay);
+// const closeBtnOverlay = document.getElementById("close-btn-overlay");
+// //console.log(closeBtnOverlay);
+// const overlayContent = document.querySelector(".overlay-content");
+// //console.log((overlayContent));
+
+
+// //CREATE ELEMENT
 
 // axios
 //     .get("https://lanciweb.github.io/demo/api/pictures/")
 //     .then((resp) => {
 //         const cardsArray = resp.data;
-//         console.log(cardsArray);
+//         //console.log(cardsArray);
 
-//         let cardString = "";
-//         cardsArray.forEach((card) => {
-//             cardString +=
-//             `  
-//                 <div class="col">
-//                     <div class="card">
-//                         <img class="card-img" src="${card.url}" alt="">
-//                         <h3 class="card-date">${card.date}</h3>
-//                         <h2 class="card-title">${card.title}</h2>
-//                     </div>
-//                 </div>
-//             `
-//         });
-//         cardGridElem.innerHTML = cardString;
+//         printCard(cardsArray);
 //     })
+
+
+// //Function print card
+// function printCard(cardsArray) {
+//     cardsArray.forEach((currentCard) => {
+
+//         const cardElem = createCard(currentCard);
+//         //console.log(currentCard);
+//         cardGridElem.append(cardElem);
+//     });
+
+// }
+
+// //Function create card
+// function createCard(currentCard) {
+//     const colElem = document.createElement("div");
+//     colElem.classList.add("col");
+
+//     colElem.innerHTML =
+//         `  
+//                  <div class="card">
+//                     <img class="pin" src="./img/pin.svg" alt="pin">
+//                     <img class="card-img" src="${currentCard.url}" alt="Image of blog activities">
+//                     <h3 class="card-date">${currentCard.date}</h3>
+//                     <h2 class="card-title">${currentCard.title}</h2>
+//                 </div>
+//             `;
+//     colElem.addEventListener("click", function () {
+//         fillOverlay(colElem);
+//         showOverlay();
+//     });
+
+//     return colElem;
+// }
+
+
+// //DAY 2
+
+// //OVERLAY:
+
+// //Function fill overlay with active card info
+// function fillOverlay(cardElem) {
+//     const imgSrc = cardElem.querySelector(".card-img").src;
+//     const title = cardElem.querySelector(".card-title").textContent;
+//     const date = cardElem.querySelector(".card-date").textContent;
+
+//     const overlayContent = document.querySelector(".overlay-content");
+//     overlayContent.innerHTML = `
+       
+//                     <img class="card-img" src="${imgSrc}" alt="Image of blog activities">
+//                     <h3 class="card-date">${date}</h3>
+//                     <h2 class="card-title">${title}</h2>
+             
+//     `;
+// }
+
+
+// //Function to show the overlaid card
+// function showOverlay() {
+//     overlayElem.classList.remove("hidden");
+// }
+
+// //On click the overlayed card dissapears (display-none)
+// closeBtnOverlay.addEventListener("click", function () {
+//     overlayElem.classList.add("hidden");
+// });
+
+
+// //Adding display none to the overlay card if any click is done around it, on background
+// overlayElem.addEventListener("click", function (event) {
+//     if (event.target === overlayElem) {  
+//         overlayElem.classList.add("hidden");
+//     }
+// });
+
+
+
+
+// //TEMPLATE LITERAL
+
+
+// // axios
+// //     .get("https://lanciweb.github.io/demo/api/pictures/")
+// //     .then((resp) => {
+// //         const cardsArray = resp.data;
+// //         console.log(cardsArray);
+
+// //         let cardString = "";
+// //         cardsArray.forEach((card) => {
+// //             cardString +=
+// //             `  
+// //                 <div class="col">
+// //                     <div class="card">
+// //                         <img class="card-img" src="${card.url}" alt="">
+// //                         <h3 class="card-date">${card.date}</h3>
+// //                         <h2 class="card-title">${card.title}</h2>
+// //                     </div>
+// //                 </div>
+// //             `
+// //         });
+// //         cardGridElem.innerHTML = cardString;
+// //     })
 
 
 
